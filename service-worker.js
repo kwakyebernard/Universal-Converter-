8df92df2f46ca5f3d013d7e01f36b547391717a0
kwakyebernard <kwakyebernard100@gmail.com>
2025-12-30 13:28:58 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+// Name of the cache
+const CACHE_NAME = "universal-converter-v1";
+
+// Files to cache for offline use
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./click.mp3"
+];
+
+/* =========================
+   INSTALL EVENT
+   ========================= */
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+/* =========================
+   ACTIVATE EVENT
+   ========================= */
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+/* =========================
+   FETCH EVENT
+   ========================= */
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
